ac71c46b49fe0b293803a142855b661111855cb2
el1syum <kilmol606@gmail.com>
2023-07-29 02:06:27 +0300

    xml

--- main.py
+++ main.py
@@ -1,33 +1,38 @@
 import asyncio
-from urllib.parse import urljoin
+import random
 import xml.etree.ElementTree as ET
+from urllib.parse import urljoin
 
 import aiohttp
-import requests
 from bs4 import BeautifulSoup
 
 DOMAIN = 'https://www.inpearls.ru'
-FILE_NAME = 'inpearls.xml'
+FILE_NAME = 'inpearls3.xml'
 ENCODING = 'UTF-8'
 
 
-async def get_urls():
-    urls = []
-    for page in range(1, 2):
-        r = requests.get(f'{DOMAIN}/authors/list-famous?page={page}')
-        soup = BeautifulSoup(r.text, 'lxml')
+async def get_urls(session):
+    urls_list = []
+    for page in range(15, 30):
+        r = await session.get(f'{DOMAIN}/authors/list-famous?page={page}')
+        soup = BeautifulSoup(await r.text(), 'lxml')
         urls = list(map(lambda i: urljoin(DOMAIN, i.find('a', class_='stretched-link').get('href')),
                         soup.find_all('div', attrs={'class': 'd-flex position-relative border-bottom py-3'})))
-        urls.extend(urls)
-    return urls
+        urls_list.extend(urls)
+    return urls_list
 
 
 async def scrap(url, session, xml_doc):
+    await asyncio.sleep(random.randint(1, 5))
     pearls = []
     page = 1
     enough = False
     while not enough:
         r = await session.get(f'{url}/page/{page}')
+        print(r.status)
+        if r.status != 200:
+            await asyncio.sleep(1)
+            continue
         soup = BeautifulSoup(await r.text(), 'lxml')
         try:
             cards = soup.find('div', class_='pearls').findChildren('div', class_='pearl')
@@ -45,11 +50,13 @@ async def scrap(url, session, xml_doc):
                     ET.SubElement(item, 'content:encoded').text = f'<![CDATA[{text}]]>'
                     ET.SubElement(item, 'wp:post_type').text = '<![CDATA[post]]>'
                     ET.SubElement(item, 'wp:status').text = '<![CDATA[publish]]>'
-                    ET.SubElement(item, 'category', domain='post_tag', nicename='stihi').text = '<![CDATA[Стихи]]>'
+                    ET.SubElement(item, 'category', domain='post_tag', nicename='citaty').text = '<![CDATA[Цитаты]]>'
                 except AttributeError:
                     ...  # empty pearl
         except AttributeError as e:
             print(url, e)
+            with open(f'index.html', 'w', encoding='utf-8') as file:
+                file.write(await r.text())
 
         page += 1
 
@@ -62,8 +69,8 @@ async def gather_data():
     ET.SubElement(author, 'wp:author_id').text = '0'
 
     tasks = []
-    urls = await get_urls()
     session = aiohttp.ClientSession()
+    urls = await get_urls(session)
     for url in urls:
         task = asyncio.create_task(scrap(url, session, xml_doc))
         tasks.append(task)
